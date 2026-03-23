04de0cc3ca02986aaffde825348b51fac10d091a
odrya-keri-mako <odry.attila@keri.mako.hu>
2026-03-23 14:00:14 +0100

    initial commit

---
+++ keri_library_vue_express/src/components/AppFooter.vue
@@ -0,0 +1,12 @@
+<template>
+  <footer class="border-top py-3">
+    <div class="container small text-secondary d-flex 
+                justify-content-between align-items-center">
+      <div>© Keri Library</div>
+      <div>
+        <i class="fa-regular fa-circle-check me-1"></i> 
+        Vue + Express
+      </div>
+    </div>
+  </footer>
+</template>
---
+++ keri_library_vue_express/src/components/AppHeader.vue
@@ -0,0 +1,39 @@
+<template>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary border-bottom sticky-top">
+    <div class="container">
+      <RouterLink class="navbar-brand fw-semibold" to="/">
+        <i class="fa-solid fa-book-open me-2"></i> 
+        Könyvtár
+      </RouterLink>
+
+      <button class="navbar-toggler" 
+              type="button" 
+              data-bs-toggle="collapse" 
+              data-bs-target="#navMain">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <div class="collapse navbar-collapse" 
+           id="navMain">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <RouterLink class="nav-link" to="/">
+              <i class="fa-solid fa-house me-1"></i> 
+              Kezdőlap
+            </RouterLink>
+          </li>
+          <li class="nav-item">
+            <RouterLink class="nav-link" to="/books">
+              <i class="fa-solid fa-book me-1"></i> 
+              Könyvek
+            </RouterLink>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
